915ecfc95cdbb55eb6120e26997d3fb9968e2e60
Maximilian Meinhardt <maximilian.meinhardt@stud.h-da.de>
2026-01-31 01:12:40 +0100

    feat: first player controller and inputs

---
+++ Assets/_Project/Scripts/Runtime/Gameplay/Player/PlayerMovement.cs
@@ -0,0 +1,76 @@
+using System;
+using GGJ.Utility.Extensions;
+using QuakeLR;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using PlayerInput = GGJ.Inputs.PlayerInput;
+
+namespace GGJ.Gameplay.Player
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField] private float moveSpeed = 10;
+
+        [SerializeField] private float lookSpeed = 5;
+
+        [SerializeField] private Vector2 maxAngles = new(-80, 80);
+        
+        [SerializeField] private Transform cameraChild;
+        
+        private QuakeCharacterController _quakeController;
+        
+        private void Awake()
+        {
+            _quakeController = GetComponent<QuakeCharacterController>();
+        }
+
+        private void OnEnable()
+        {
+            PlayerInput.Input.Player.Jump.performed += OnJump;
+        }
+
+        private void OnDisable()
+        {
+            PlayerInput.Input.Player.Jump.performed -= OnJump;
+        }
+        
+        private void OnJump(InputAction.CallbackContext context)
+        {
+            _quakeController.TryJump();
+        }
+
+        private void Update()
+        {
+            HandleCameraRotation();
+            HandleMovement();
+        }
+
+
+        private void HandleMovement()
+        {
+            Vector2 moveInput = PlayerInput.Move;
+            Vector3 moveDir = transform.rotation * new Vector3(moveInput.x, 0, moveInput.y);
+            
+            _quakeController.Move(moveDir);
+            _quakeController.ControllerThink(Time.deltaTime);
+        }
+
+
+        private void HandleCameraRotation()
+        {
+            Vector2 lookInput = PlayerInput.Look;
+            
+            transform.Rotate(Vector3.up, lookInput.x * lookSpeed * Time.deltaTime);
+
+            float pitch = cameraChild.localEulerAngles.x;
+            pitch -= lookInput.y * lookSpeed * Time.deltaTime;
+
+            if (pitch > 180)
+                pitch -= 360;
+            
+            pitch = Mathf.Clamp(pitch, maxAngles.x, maxAngles.y);
+
+            cameraChild.localEulerAngles = new(pitch, 0, 0);
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Runtime/Inputs/PlayerInput.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+namespace GGJ.Inputs
+{
+    [DefaultExecutionOrder(-100)]
+    public class PlayerInput : MonoBehaviour
+    {
+        public static PlayerInput Instance;
+
+
+        private static PlayerInputActions _input;
+        
+        public static PlayerInputActions Input
+        {
+            get
+            {
+                if (_input == null)
+                    InitInputs();
+                return _input;
+            }
+            private set
+            {
+                _input = value;
+            }
+        }
+
+
+
+        #region Properties
+
+        public static Vector2 Move => Input.Player.Move.ReadValue<Vector2>();
+        public static Vector2 Look => Input.Player.Look.ReadValue<Vector2>();
+
+        #endregion
+
+        private static void InitInputs()
+        {
+            _input = new PlayerInputActions();
+            _input.Enable();
+        }
+
+        private void Awake()
+        {
+            Instance = this;
+            InitInputs();
+
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        private void OnDestroy()
+        {
+            if (Instance == this)
+                Instance = null;
+        }
+    }
+}
